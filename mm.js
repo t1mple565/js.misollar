dd7d40576e50219b151625099c1dd66fb3ba74d8
WVMN <abbossabiryozov6@gmail.com>
2025-11-03 14:48:56 +0500

    .
    ..

---
+++ mm.js
@@ -0,0 +1,171 @@
+// 01
+
+// let z = 1
+// x = z ** 3
+// console.log(x)
+
+// let y = 3
+// x = y ** 3
+// console.log(x)
+
+// 02
+
+// const r1 = 3
+// const r2 = 7
+// const r3 = 1
+
+// const x1 = Math.PI * r1 ** 2
+// const x11 = x1.toFixed(2)
+// console.log (x11)
+
+
+// 03
+// const s = 8
+// const h = 5
+// const a = 2 * s / h 
+// const ab = a.toFixed(2)
+// console.log (ab)
+
+// 04
+
+// const r1 = 2
+// const a = 4 * Math.PI * (r1 ** 2)
+// const ab = a.toFixed(2)
+// console.log(ab)
+
+ 
+// 05
+
+// const a = 6
+// const b = 6
+// const c = 9 
+// const p = a + b + c
+// const yp = p / 2
+// const x = yp.toFixed(2)
+// console.log(x)
+
+
+// 06 ?
+
+// const a = 2
+// const b = 9
+// const h = 8
+// const x =  ((a + b) * h ) / 2
+// const s = x.toFixed(2)
+// console.log(s) 
+
+// 07
+ 
+// const r = 5 
+// const h = 3
+// const x = (1/3) * Math.PI * (r ** 2) * h
+// const v = x.toFixed(2)
+// console.log(v)
+
+
+// 08
+// const v = 6
+// const s = 2
+// const x = s / v
+// const t = x.toFixed(2)
+// console.log(t)
+
+
+// 09
+// const g = 9.8
+// const h = 2 
+// const x = Math.sqrt((2 * h) / g)
+// const s = x.toFixed(2)
+// console.log(s)
+
+
+// 10
+// const y = 2 
+// const j = 31536
+// const x = j * y 
+// console.log(x)
+
+
+// 11
+
+// const n = 4
+// const d = n * (n + 1) / 2
+// console.log(d)
+
+
+// 12
+// const g = 9.8
+// const m = 6
+// const p = m * g
+// const x = p.toFixed(2)
+// console.log(x)
+
+
+// 13
+// const a = 2
+// const m = 8
+// const f = m * a
+// console.log(f)
+
+
+// 14
+// const u = 8
+// const r = 7
+// const t =  u / r
+// const i = t.toFixed(3)
+// console.log(i)
+
+// 15
+// const r1 = 1 
+// const r2 = 7 
+// const r3 = 3
+// const z = 1 / ((1 / r1) + (1 / r2) + (1 / r3))
+// const x = z.toFixed(2)
+// console.log(x)
+
+
+
+// 16       ✅
+
+// const e = Math.E
+// const x = 9.79
+// const y = 6.74
+// const c = (x + y) / ((y ** 2) + Math.abs(((y ** 2) +2 ) / (x + ((x ** 3) / 5)))) + (e **( y + 2))
+// const c1 = c.toFixed(2)
+// console.log(c1)
+
+
+
+// 17 ??????????????????????????????????????????????????????????????
+// const p = Math.PI
+// const x = 0.22
+// const y = 6.17
+// const f = (2 * Math.tan(x + (p / 6))) / (1/3 + (Math.cos(y + (x ** 2)) ** 2) + (Math.log2((x ** 2) +2)))
+// const f1 = f.toFixed(2) 
+// console.log(f1)
+
+
+// 18
+// const e = Math.E          ✅
+// const x = 3.19
+// const y = 4.75
+// const f = ((1 / (x +2 / (x ** 2) + 3 / (x ** 3)) + (e ** ((x **2) + 3 * x))) / (Math.atan(x + y) + Math.abs((5 + x) ** 2)) ) - (Math.cos((y ** 2) +(x ** 2) / 2) ** 2)
+// const f2 = f.toFixed(2)
+// console.log(f2)
+
+// 19 ????????????????????????????????????????????????????????????
+// const x = 2 
+// const y = 1
+// const w = Math.log ( Math.abs((x + y) ** 2 + Math.sqrt(Math.abs(y) + 2) - (x - (x * y) / ((x **2) / 2 - 5))) + ((Math.cos(x + y)) ** 2) / (x + y) ** (1 /3) )
+// const z = w.toFixed(2)
+// console.log(z)
+
+
+// 20
+
+// const x = 7.09 ???????????????????????????????????/
+// const y = 3.92
+// const t = ((x**2) + 1) / ((x ** 2) + (x * y  + (y ** 2)) / ((y ** 2) + (y + x * y) / (Math.abs(x * y)) + 5))  +  1 / (1 + Math.cos(x) +  1  / (Math.sin(Math.abs(x))))
+// const t11 = t.toFixed(2)
+// console.log(t11)
+
