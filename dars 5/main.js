cb8f2a86c080dc62bff83c05736a0b7950c6b2a8
WVMN <abbossabiryozov6@gmail.com>
2025-11-05 16:23:02 +0500

    Add files via upload

---
+++ dars 5/main.js
@@ -0,0 +1,90 @@
+
+// const numberOfseres = +prompt("Nechta serial kordingiz ?")
+
+// const seriesDB = {
+//     count: numberOfseres,
+//     series: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// }
+// const a = prompt("Oxirgi korgan serialingiz ?")
+// const b = +prompt("Necha baxo qo'yasiz?")
+
+// const c = prompt("Oxirgi korgan serialingiz ?")
+// const d = +prompt("Necha baxo qo'yasiz?")
+
+// seriesDB.series[a] = b
+// seriesDB.series[c] = d
+
+// // const actor1 = promt("Yaxshi korgan aktiyoringiz ?")
+// // seriesDB.actors[actor1] = actor1
+
+// // const janr1 = promt("yaxshi korgan janringiz ?")
+// // seriesDB.genres[janr1] = janr1
+
+// const e = prompt("Oxirgi ko'rgan aktyoringiz?");
+// const u = prompt("Necha baho berasiz?");
+
+// const y = prompt("Oxirgi ko'rgan aktyoringiz?");
+// const z = prompt("Necha baho berasiz?");
+
+// seriesDB.actors[e] = u;
+// seriesDB.actors[y] = z; 
+
+// console.log(seriesDB);
+// console.log(seriesDB);
+
+
+
+
+
+
+
+
+
+//Shartli operotorlar
+
+// if - agarda
+// else if -aks xolda
+// elif- yokida
+
+// if (sharti){
+// qiymati
+// }
+
+// const  svetofor =  'green'
+
+// if(svetofor == 'red') {
+//     console.log('Stop');
+// }
+// else if (svetofor =='yellow') {
+//     console.log('Ready');
+// }
+// else if (svetofor == 'green') {
+//     console.log('Gooo');
+// }
+
+// const age = +prompt("How old are you ? ,'' ")
+// if (age<= 18 ){
+//     console.log("hali shoppottinson"); 
+// }
+// else {
+//     console.log("san qorribson");
+    
+// }
+
+
+const age = +prompt("How old are you ? ...")
+if(age <= 12){
+    console.log("chipta tekin");
+}
+else if (13< age <= 18){
+    console.log("chipta 10ming");
+}
+else if (19< age <= 65){
+    console.log("chipta 20 ming");
+}
+else if(66< age) {
+    console.log("chiptra tekin");
+}
